cc52b62e00fa5ca13683ae619e56dfbfc278a833
Jennaro <otto.lame@gmail.com>
2024-04-18 12:15:23 +0200

    First commit
    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: ['./**/*.html'],
+  
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
